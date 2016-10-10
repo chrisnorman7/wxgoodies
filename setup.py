40c4fcc10970ec4e54caef0859a68ea29f480601
Chris Norman <chris.norman2@googlemail.com>
2016-10-10 18:26:21 +0100

    Now use pbr for setup.

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
 from setuptools import setup
-from wxgoodies import version
 
 setup(
- name = 'wxgoodies',
- version = version,
- description = 'Helper functions for wxPython.',
- url = 'https://github.com/chrisnorman7/wxgoodies',
- author = 'Chris Norman',
- author_email = 'chris.norman2@googlemail.com',
- license = 'GPL',
- packages = ['wxgoodies'],
- keywords = ['hotkeys'],
- zip_safe = True
+    setup_requires = ['pbr', 'setuptools'],
+    pbr = True
 )
